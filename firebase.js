81cb5dd0525e29b220760a850c8270796ac97536
Cristian Andres <157153392+cristianAnAn@users.noreply.github.com>
2025-10-20 23:35:59 -0600

    Refactor Firebase admin initialization
    Update Firebase initialization to use environment variable for service account and handle private key formatting.

--- firebase.js
+++ firebase.js
@@ -1,12 +1,14 @@
-import admin from 'firebase-admin';
-import fs from 'fs';
+// firebase.js
+const admin = require('firebase-admin');
 
-const serviceAccount = JSON.parse(
-  fs.readFileSync('./secrets/serviceAccountKey.json', 'utf8')
-);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+// Si guardas la private_key con \n escapado:
+if (serviceAccount.private_key && serviceAccount.private_key.includes('\\n')) {
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-export default admin;
+module.exports = admin;
